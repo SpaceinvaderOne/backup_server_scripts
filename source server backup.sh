6177c2412e6dd6dae45dac7a570a077e09b311b2
SpaceinvaderOne <54743094+SpaceinvaderOne@users.noreply.github.com>
2022-02-20 17:45:18 +0000

    Update source server backup.sh

--- source server backup.sh
+++ source server backup.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 umask 0000
 
-startserver="smartplug"   #set to method to start backup server: etherwake (wake on lan) or smartplug
-backupip="10.10.20.197"   #set ip of the backup server
-backupmacaddress="52:54:00:33:6c:bb" # set macaddress of backup servers nic (if using etherwake) - ignored if using etherwake
-smartplugip="http://192.168.11.237" #address of tasmota smart plug - ignored if start server set to etherwake
-poweroff="backup"  #set to shutdown source server, backup server or none after sync  "none" "both" "source" "backup"
-containerstart="no" # set to "yes or "no" to tell backup server to start selected (on backup server) containers to start
-
+startserver="smartplug"   # set to choose method used to start backup server: "etherwake" (wake on lan) or "smartplug" tasmota smart switch
+smartplugip="http://192.168.11.237" # set ip address of tasmota smart plug - ignored if start server set to etherwake
+backupmacaddress="xx:xx:xx:xx:xx:xx" # set macaddress of backup servers' NIC (if using etherwake) - ignored if using smartplug
+backupip="10.10.20.197"   # set ip address of the backup server
+poweroff="source"  # set to shutdown source server, backup server or neither server after sync --  "none" "both" "source" "backup"
+vm="CentOS10"  # if this vm is running and poweroff is set to 'source' then poweroff will be set to none so source server doesnt shut down
+containerstart="yes" # set to "yes or "no" to tell backup server to start selected  containers on backup server after sync
 
 ############################# functions ########################################
 
 setup () {
 # make directory to tell backup server when it starts to start backup process
-mkdir -vp /mnt/user/appdata/backupserver/start
+mkdir -p /mnt/user/appdata/backupserver/start ; echo "Making directory in appdata for script flags"
 #set flag to shutdown backup or source server after sync
 if [ "$poweroff" == "backup"  ] ; then
-mkdir -vp /mnt/user/appdata/backupserver/backupoff
+mkdir -p /mnt/user/appdata/backupserver/backupoff
 echo "Backup server has been set to turn off after sync"
 elif [ "$poweroff" == "source"  ] ; then
-mkdir -vp /mnt/user/appdata/backupserver/sourceoff
+mkdir -p /mnt/user/appdata/backupserver/sourceoff
 echo "Source server has been set to turn off after sync"
 else
 echo "Neither Source nor Backup server is set to be turned off"
 fi
 if [ "$containerstart" == "yes"  ] ; then
-mkdir -vp /mnt/user/appdata/backupserver/containerstart
-echo "Containers set to start on Backup server after sync completed"
+mkdir -p /mnt/user/appdata/backupserver/containerstart
+echo "Containers are set to start on Backup server after sync completed"
 else
-echo "No containers on Backup server set to start after sync had completed"
+echo "No containers on Backup server are set to start after sync had completed"
+fi
 }
 
 wakeonlan () {
@@ -47,24 +48,47 @@ shallicontinue () {
   fi
 }
 
+vmcheck () {
+echo "Checking if VM is running"
+vmstate=$(virsh list --all | grep " $vm " | awk '{ print $3}')
+if [ "$vmstate" == "x" ] || [ "$vmstate" != "running" ]
+then
+    echo "VM is not running, so source server will shut down after sync"
+else
+    echo "VM is running so source server will not shut down after sync"
+    echo "Setting shutdown state to neither source or backup to shutdown after sync"
+    poweroff="none"
+fi
+
+}
+
 backupserverstatus () {
 #check if backup server has started up yet
+checkbackup=1
 while [ "$backupserverstatus" == "off" ]
 do
   checkbackupserver
+  echo ".............................Checking backup server attempt...""$checkbackup"
   echo "Backup server not started yet"
+  echo "Waiting 30 seconds to check again"
+  ((checkbackup=checkbackup+1))
   sleep 30  # wait 30 seconds before rechecking
 done
 echo "Okay server is now on, backup process should start from the backup server side"
 
 if [ "$poweroff" == "backup"  ] ; then
+checkbackup=1
 #check if backup server has shutdown
 while [ "$backupserverstatus" == "on" ]
 do
   checkbackupserver
-  echo "Backup server is still on"
-  sleep 60  # wait 60 seconds before rechecking
+  echo ".............................Checking backup server attempt...""$checkbackup"
+  echo "Backup server not shutdown"
+  echo "Waiting 30 seconds to check again"
+ ((checkbackup=checkbackup+1))
+  sleep 30  # wait 30 seconds before rechecking
 done
+
 smartplugoff
 echo "backup server is down and smartplug powered off"
 
@@ -106,6 +130,7 @@ fi
 ############################## start process ###################################
 if [ "$startserver" == "smartplug" ] ; then
 shallicontinue
+vmcheck
 setup
 smartplugoff
 sleep 5
@@ -113,6 +138,7 @@ smartplugon
 backupserverstatus
 else
 shallicontinue
+vmcheck
 setup
 wakeonlan
 backupserverstatus
