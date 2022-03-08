ddbec6927f86826367df729a7c0a3f85cbed4fff
SpaceinvaderOne <54743094+SpaceinvaderOne@users.noreply.github.com>
2022-03-08 23:52:56 +0000

    updated to use ssh keys and rsync

--- backup server backup.sh
+++ backup server backup.sh
@@ -1,130 +1,205 @@
 #!/bin/bash
-umask 0000
-
 # backup server script -- needs source server script setup on source server to work
-
+umask 0000
 ############# Basic settings ##############################################################
-# these must be set
-mountlocation="/mnt/remotes/prime"         # set remote mounted location (make sure NOT to have a  / on end)
-
-sourcelocation1="/mnt/remotes/prime/Movies/"    # set first source directory to sync
-backuplocation1="/mnt/user/Movies/"             # destination location for above
-sourcelocation2="/mnt/remotes/prime/TV Shows/"  # set second source directory to sync
-backuplocation2="/mnt/user/TV Shows/"           # destination location for above
-
-# remove '#' and fill in to add locations below for more directories to sync
-# also remove '#' for each new location under function 'syncdata' in basic functions
-
-# sourcelocation3="xxxxxxxxxxxxxxxx"            # set third source directory to sync
-# backuplocation3="xxxxxxxxxxxxxxxx"            # destination location for above
-# sourcelocation4="xxxxxxxxxxxxxxxx"            # set fourth source directory to sync
-# backuplocation4="xxxxxxxxxxxxxxxx"            # destination location for above
+source_server_ip="192.168.1.100" # set to the ip of the source server
+forcestart="no"  # default is "no" - set to yes to force process to run even if source server didn't request
 
 
-remotelocation="$mountlocation"/appdata/backupserver  # do not change
+HOST="root@""$source_server_ip" # dont change
+CONFI="/mnt/user/appdata/backupserver/config.cfg" # dont change
 
-############# Basic functions ##############################################################
+#############  Functions ##############################################################
 
-# this function syncs data from source server to backup server
-syncdata() {
-# the locations below will be synced (these are defined in basic settings)
-rsync -avhP --delete "$sourcelocation1" "$backuplocation1"    # first location to sync
-rsync -avhP --delete "$sourcelocation2" "$backuplocation2"    # second location to sync
-
-# rsync -avhP --delete "$sourcelocation3" "$backuplocation3"  # third location to sync
-# rsync -avhP --delete "$sourcelocation4" "$backuplocation4"  # forth location to sync
-# add additional locations if needed
+# read config file written by source server and set other variables
+readconfig () {
+mkdir -p /mnt/user/appdata/backupserver/
+rsync -avhsP  "$HOST":"$CONFI" "$CONFI" 
+source "$CONFI"
+ssh "$HOST" [[ -f /mnt/user/appdata/backupserver/start ]] && start="yes" ||  start="no";
 }
 
-# this function checks the power off setting which is set on source script on source server
-shutdownstatus () {
-if [ -d $remotelocation/backupoff ] ; then
-poweroff="backup"
-containerstart="no" 
-rm -r $remotelocation/backupoff
-elif [ -d $remotelocation/sourceoff ] ; then
-poweroff="source"
-rm -r $remotelocation/sourceoff
-else
-poweroff="none"
+#####################
+
+# sync data from source server to backup server
+syncmaindata () {
+if [ "$copymaindata" == "yes"  ] ; then
+# the locations below will be synced (these are defined in basic settings)
+if [ "$source1" != "" ] && [ "$destination1" != ""  ] ; then
+rsync -avhsP --delete  "$HOST":"$source1" "$destination1"    # first location to sync
+fi
+if [ "$source2" != "" ] && [ "$destination2" != ""  ] ; then
+rsync -avhsP --delete  "$HOST":"$source2" "$destination2"    # second location to sync
+fi
+if [ "$source3" != "" ] && [ "$destination3" != ""  ] ; then
+rsync -avhsP --delete "$HOST":"$source3" "$destination3"  # third location to sync
+fi
+if [ "$source4" != "" ] && [ "$destination4" != ""  ] ; then
+rsync -avhsP --delete "$HOST":"$source4" "$destination4"  # forth location to sync
+fi
+if [ "$source5" != "" ] && [ "$destination5" != ""  ] ; then
+rsync -avhsP --delete "$HOST":"$source5" "$destination5"    # fifth location to sync
+fi
+if [ "$source6" != "" ] && [ "$destination6" != ""  ] ; then
+rsync -avhsP --delete "$HOST":"$source6" "$destination6"    # sixth location to sync
+fi
+if [ "$source7" != "" ] && [ "$destination7" != ""  ] ; then
+rsync -avhsP --delete "$HOST":"$source7" "$destination7"  # seventh location to sync
+fi
+if [ "$source8" != "" ] && [ "$destination8" != ""  ] ; then
+rsync -avhsP --delete "$HOST":"$source8" "$destination8"  # eighth location to sync
+fi
+if [ "$source9" != "" ] && [ "$destination9" != ""  ] ; then
+rsync -avhsP --delete "$HOST":"$source9" "$destination9"  # ninth location to sync
+fi
+
 fi
 }
 
+#####################
+
+syncappdata () {
+if [ "$copyappdata" == "yes"  ] ; then
+shutdowncontainersbackup
+shutdowncontainerssource 
+
+if [ "$appsource1" != "" ] && [ "$appdestination1" != ""  ] ; then
+rsync -avhsP --delete "$HOST":"$appsource1" "$appdestination1"    # first appdata location to sync
+fi
+if [ "$appsource2" != "" ] && [ "$appdestination2" != ""  ] ; then
+rsync -avhsP --delete "$HOST":"$appsource2" "$appdestination2"    # second appdata location to sync
+fi
+if [ "$appsource3" != "" ] && [ "$appdestination3" != ""  ] ; then
+rsync -avhsP --delete "$HOST":"$appsource3" "$appdestination3"  # third appdata location to sync
+fi
+if [ "$appsource4" != "" ] && [ "$appdestination4" != ""  ] ; then
+rsync -avhsP --delete "$HOST":"$appsource4" "$appdestination4"  # forth appdata location to sync
+fi
+if [ "$appsource5" != "" ] && [ "$appdestination5" != ""  ] ; then
+rsync -avhsP --delete "$HOST":"$appsource5" "$appdestination5"    # fifth appdata location to sync
+fi
+if [ "$appsource6" != "" ] && [ "$appdestinatio62" != ""  ] ; then
+rsync -avhsP --delete "$HOST":"$appsource6" "$appdestination6"    # sixth appdata location to sync
+fi
+if [ "$appsource7" != "" ] && [ "$appdestination7" != ""  ] ; then
+rsync -avhsP --delete "$HOST":"$appsource7" "$appdestination7"  # seventh appdata location to sync
+fi
+if [ "$appsource8" != "" ] && [ "$appdestination8" != ""  ] ; then
+rsync -avhsP --delete "$HOST":"$appsource8" "$appdestination8"  # eighth appdata location to sync
+fi
+if [ "$appsource9" != "" ] && [ "$appdestination9" != ""  ] ; then
+rsync -avhsP --delete "$HOST":"$appsource9" "$appdestination9"  # ninth appdata location to sync
+fi
+
+startupcontainers
+fi
+}
+
+#####################
+
 # this function cleans up and exits script shutting down server if that has been set
 endandshutdown () {
-rm -r $remotelocation/start  # delete 'start' folder created by main server now process has finished
 
 if [ "$poweroff" == "backup"  ] ; then
 echo "Shutting down backup server"
 poweroff # shutdown backup server
 
 elif [ "$poweroff" == "source"  ] ; then
-mkdir -vp $remotelocation/sourceoffnow  # create flag for source server to shutdown
+ssh "$HOST" 'poweroff'  # shutdown source server to shutdown
 echo "source server will shut off shortly"
+touch /mnt/user/appdata/backupserver/i_shutdown_source_server
 
 else
 echo "Neither Source nor backup server set to turn off"
 fi
+
+ssh "$HOST" 'rm /mnt/user/appdata/backupserver/start' 
+
 }
+
+#####################
 
 # this function plays completion tune when sync finished (will not work without beep speaker)
 completiontune () {
 beep -l 600 -f 329.627556913 -n -l 400 -f 493.883301256 -n -l 200 -f 329.627556913 -n -l 200 -f 493.883301256 -n -l 200 -f 659.255113826 -n -l 600 -f 329.627556913 -n -l 400 -f 493.883301256 -n -l 200 -f 329.627556913 -n -l 200 -f 493.883301256 -n -l 200 -f 659.255113826 -n -l 600 -f 329.627556913 -n -l 360 -f 493.883301256 -n -l 200 -f 329.627556913 -n -l 200 -f 493.883301256 -n -l 640 -f 659.255113826 -n -l 160 -f 622.253967444 -n -l 200 -f 329.627556913 -n -l 200 -f 554.365261954 -n -l 200 -f 329.627556913 -n -l 200 -f 622.253967444 -n -l 200 -f 493.883301256 -n -l 200 -f 830.60939516 -n -l 200 -f 415.30469758 -n -l 80 -f 739.988845423 -n -l 40 -f 783.990871963 -n -l 80 -f 739.988845423 -n -l 200 -f 415.30469758 -n -l 200 -f 659.255113826 -n -l 200 -f 622.253967444 -n -l 400 -f 554.365261954 -n -l 1320 -f 415.30469758 -n -l 40 -f 7458.62018429 -n -l 40 -f 7040.0 -n -l 40 -f 4186.00904481 -n -l 40 -f 3729.31009214 -n -l 40 -f 6644.87516128 -n -l 40 -f 7902.1328201 -n -l 40 -f 16.3515978313 -n -l 200 -f 830.60939516 -n -l 200 -f 415.30469758 -n -l 40 -f 739.988845423 -n -l 80 -f 783.990871963 -n -l 80 -f 739.988845423 -n -l 200 -f 415.30469758 -n -l 200 -f 659.255113826 -n -l 200 -f 622.253967444 -n -l 400 -f 554.365261954 -n -l 1320 -f 415.30469758 -n -l 40 -f 4698.63628668
 }
 
+#####################
 
-############# Advanced settings ###########################################################
-#optional - do not need to be set
+startupcontainers() {
 
-copyappdata="yes"         # set to copy appdata for specific containers to backup server
+if [ "$switchserver" == "yes"  ] ; then
 
-dockersource1="/mnt/remotes/prime/appdata/EmbyServer/data/"    # set first appdata source directory to sync
-dockerbackup1="/mnt/user/appdata/EmbyServer/data/"             # destination location for above
+for contval in "${container_start_stop[@]}"
+do
+   echo "Starting specified containers on backup server ....." 
+   docker start "$contval"
+   echo 
+done
+fi
 
-# remove '#' and fill in to add locations below for more directories to sync
-# also remove '#' for each new location under function 'syncdata' in basic functions
-# dockersource2="/mnt/remotes/prime/TV Shows/"  # set second appdata source directory to sync
-# dockerbackup2="/mnt/user/TV Shows/"           # destination location for above
-# dockersource3="xxxxxxxxxxxxxxxx"              # set third appdata source directory to sync
-# dockerbackup3="xxxxxxxxxxxxxxxx"              # destination location for above
-# dockersource4="xxxxxxxxxxxxxxxx"              # set fourth appdata source directory to sync
-# dockerbackup4="xxxxxxxxxxxxxxxx"              # destination location for above
+if [ "$switchserver" == "no"  ] ; then
 
-
-############# Advanced functions ##############################################################
-
-copyappdata () {
-if [ -d $remotelocation/containerstart ] ; then
-rm -r $remotelocation/containerstart # delete flag
-# the locations below will be synced (these are defined in advanced settings)
-rsync -avhP --delete   "$dockerbackup1" "$dockersource1" # first appdata to sync
-
-# rsync -avhP --delete "$dockersource2" "$dockerbackup2"    # second appdata to sync
-# rsync -avhP --delete "$dockersource3" "$dockerbackup3"   # third appdata to sync
-# rsync -avhP --delete "$dockersource4" "$dockerbackup4"  # forth appdata to sync
-# add additional locations if needed
-
-# start containers
-docker start EmbyServerBeta # change container to startup to suit
-docker start swag
-# add other conatiners to suit
+for contval in "${container_start_stop[@]}"
+do
+   echo "Starting specified containers on source server  ....." 
+  ssh "$HOST" docker start "$contval"
+   echo 
+done
 fi
 }
 
+#####################
 
-############# Start process ###################################################################
+shutdowncontainerssource() {
 
-# check if main server started process by making start directory, then start sync
-sleep 30   #waits to be sure unassigned devices has made connection to source server
-if [ -d $remotelocation/start ] ; then
-shutdownstatus
-syncdata
-copyappdata
+for contval in "${container_start_stop[@]}"
+do
+  echo "Shutting down specified containers on host ....." 
+  ssh "$HOST" docker stop "$contval"
+  echo 
+done
+sleep 10
+}
+
+#####################
+
+shutdowncontainersbackup() {
+
+for contval in "${container_start_stop[@]}"
+do
+  echo "Shutting down specified containers on backup server before sync ....." 
+  docker stop "$contval"
+  echo 
+done
+sleep 10
+}
+
+#####################
+
+mainfunction () {
+# check if main server started process by making start flag file, then start sync
+if [ "$start" == "yes" ] ; then
+syncmaindata 
+syncappdata 
 completiontune
-endandshutdown
+endandshutdown 
+
+elif  [ "$forcestart" == "yes"  ] ; then
+syncmaindata 
+syncappdata 
+completiontune
+endandshutdown 
 
 else
 echo "Source server didn't request sync job"
 echo "Normal start of backup server so exiting script"
-
+exit
 fi
+}
+
+############# Start process ###################################################################
+
+readconfig
+mainfunction 2>&1 | ssh "$HOST" -T tee -a "$logname"
+exit
